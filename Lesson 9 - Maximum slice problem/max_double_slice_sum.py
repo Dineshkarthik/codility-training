ebe1bb7b4776d047dfa334b4cb44e9f3bc2c1dbd
Dineshkarthik <dinesh.kartoon@gmail.com>
2017-08-25 12:03:04 +0530

    ADD: Max Slice Problem

---
+++ Lesson 9 - Maximum slice problem/max_double_slice_sum.py
@@ -0,0 +1,74 @@
+"""
+A non-empty zero-indexed array A consisting of N integers is given.
+
+A triplet (X, Y, Z), such that 0 ≤ X < Y < Z < N, is called a double slice.
+
+The sum of double slice (X, Y, Z) is the total of
+ A[X + 1] + A[X + 2] + ... + A[Y − 1] + A[Y + 1] + A[Y + 2] + ... + A[Z − 1].
+
+For example, array A such that:
+
+    A[0] = 3
+    A[1] = 2
+    A[2] = 6
+    A[3] = -1
+    A[4] = 4
+    A[5] = 5
+    A[6] = -1
+    A[7] = 2
+contains the following example double slices:
+
+double slice (0, 3, 6), sum is 2 + 6 + 4 + 5 = 17,
+double slice (0, 3, 7), sum is 2 + 6 + 4 + 5 − 1 = 16,
+double slice (3, 4, 5), sum is 0.
+The goal is to find the maximal sum of any double slice.
+
+Write a function:
+
+def solution(A)
+
+that, given a non-empty zero-indexed array A consisting of N integers,
+ returns the maximal sum of any double slice.
+
+For example, given:
+
+    A[0] = 3
+    A[1] = 2
+    A[2] = 6
+    A[3] = -1
+    A[4] = 4
+    A[5] = 5
+    A[6] = -1
+    A[7] = 2
+the function should return 17,
+ because no double slice of array A has a sum of greater than 17.
+
+Assume that:
+
+N is an integer within the range [3..100,000];
+each element of array A is an integer within the range [−10,000..10,000].
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(N),
+ beyond input storage (not counting the storage required for input arguments).
+Elements of input arrays can be modified.
+"""
+
+import sys
+
+
+def solution(A):
+    n = len(A)
+    left = [0] * n
+    for i in xrange(1, n - 1):
+        left[i] = max(0, left[i - 1] + A[i])
+
+    right = [0] * n
+    for i in xrange(n - 2, 1, -1):
+        right[i] = max(0, right[i + 1] + A[i])
+
+    max_sum = -sys.maxint
+    for i in xrange(1, n - 1):
+        max_sum = max(max_sum, left[i - 1] + right[i + 1])
+    return max_sum
