c6696101887356cf680fb803dbc02be90001027c
Dineshkarthik Raveendran <dineshkarthik@theviralfever.com>
2017-08-29 18:26:59 +0530

    UPD: MinPerimeterRectangle

--- Lesson 10 - Prime and composite numbers/min_perimeter_rectangle.py
+++ Lesson 10 - Prime and composite numbers/min_perimeter_rectangle.py
@@ -29,3 +29,18 @@ Complexity:
 expected worst-case time complexity is O(sqrt(N));
 expected worst-case space complexity is O(1).
 """
+
+
+def solution(N):
+    result = 0
+    if N > 0:
+        i = 1
+        perimeters = []
+        while i * i <= N:
+            if N % i == 0:
+                perimeters.append(2 * (i + (N / i)))
+                i = i + 1
+                continue
+            i += 1
+        result = min(perimeters)
+    return result
