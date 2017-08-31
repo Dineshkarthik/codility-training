46a81346b8b5cc3563bb0c4c76f837fe2316ebd8
Dineshkarthik <dinesh.kartoon@gmail.com>
2017-08-31 12:08:53 +0530

    ADD: Peaks

---
+++ Lesson 10 - Prime and composite numbers/peaks.py
@@ -0,0 +1,132 @@
+"""
+A non-empty zero-indexed array A consisting of N integers is given.
+
+A peak is an array element which is larger than its neighbors.
+More precisely, it is an index P such that 0 < P < N − 1,
+ A[P − 1] < A[P] and A[P] > A[P + 1].
+
+For example, the following array A:
+
+    A[0] = 1
+    A[1] = 2
+    A[2] = 3
+    A[3] = 4
+    A[4] = 3
+    A[5] = 4
+    A[6] = 1
+    A[7] = 2
+    A[8] = 3
+    A[9] = 4
+    A[10] = 6
+    A[11] = 2
+has exactly three peaks: 3, 5, 10.
+
+We want to divide this array into blocks containing the same number of elements.
+More precisely, we want to choose a number K that will yield the following blocks:
+
+A[0], A[1], ..., A[K − 1],
+A[K], A[K + 1], ..., A[2K − 1],
+...
+A[N − K], A[N − K + 1], ..., A[N − 1].
+What's more, every block should contain at least one peak.
+Notice that extreme elements of the blocks (for example A[K − 1] or A[K])
+can also be peaks, but only if they have both neighbors
+(including one in an adjacent blocks).
+
+The goal is to find the maximum number of blocks into which the array A can be divided.
+
+Array A can be divided into blocks as follows:
+
+one block (1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2). This block contains three peaks.
+two blocks (1, 2, 3, 4, 3, 4) and (1, 2, 3, 4, 6, 2). Every block has a peak.
+three blocks (1, 2, 3, 4), (3, 4, 1, 2), (3, 4, 6, 2).
+Every block has a peak. Notice in particular that the first block (1, 2, 3, 4)
+has a peak at A[3], because A[2] < A[3] > A[4], even though A[4] is in the adjacent block.
+However, array A cannot be divided into four blocks, (1, 2, 3), (4, 3, 4), (1, 2, 3)
+and (4, 6, 2), because the (1, 2, 3) blocks do not contain a peak.
+Notice in particular that the (4, 3, 4) block contains two peaks: A[3] and A[5].
+
+The maximum number of blocks that array A can be divided into is three.
+
+Write a function:
+
+def solution(A)
+
+that, given a non-empty zero-indexed array A consisting of N integers,
+ returns the maximum number of blocks into which A can be divided.
+
+If A cannot be divided into some number of blocks, the function should return 0.
+
+For example, given:
+
+    A[0] = 1
+    A[1] = 2
+    A[2] = 3
+    A[3] = 4
+    A[4] = 3
+    A[5] = 4
+    A[6] = 1
+    A[7] = 2
+    A[8] = 3
+    A[9] = 4
+    A[10] = 6
+    A[11] = 2
+the function should return 3, as explained above.
+
+Assume that:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [0..1,000,000,000].
+Complexity:
+    * expected worst-case time complexity is O(N*log(log(N)));
+    * expected worst-case space complexity is O(N),
+       beyond input storage (not counting the storage required for input arguments).
+Elements of input arrays can be modified.
+"""
+
+
+def solution(A):
+
+    length = len(A)
+
+    # array ends can't be peaks, len < 3 must return 0
+    if len < 3:
+        return 0
+
+    peaks = [0] * length
+
+    # compute a list of 'peaks to the left' in O(n) time
+    for index in range(2, length):
+        peaks[index] = peaks[index - 1]
+
+        # check if there was a peak to the left, add it to the count
+        if A[index - 1] > A[index - 2] and A[index - 1] > A[index]:
+            peaks[index] += 1
+
+    # candidate is the block size we're going to test
+    for candidate in range(3, length + 1):
+
+        # skip if not a factor
+        if length % candidate != 0:
+            continue
+
+        # test at each point n / block
+        valid = True
+        index = candidate
+        while index != length:
+
+            # if no peak in this block, break
+            if peaks[index] == peaks[index - candidate]:
+                valid = False
+                break
+
+            index += candidate
+
+        # one additional check since peaks[length] is outside of array
+        if index == length and peaks[index - 1] == peaks[index - candidate]:
+            valid = False
+
+        if valid:
+            return length / candidate
+
+    return 0
