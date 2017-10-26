bb7d9b6115b5985456e2ea033b866297f58ec442
Dineshkarthik <dinesh.kartoon@gmail.com>
2017-10-26 16:16:34 +0530

    UPD: Pep8

--- Lesson 05 - Prefix Sums/genomic_range_query.py
+++ Lesson 05 - Prefix Sums/genomic_range_query.py
@@ -29,7 +29,7 @@ The part between positions 0 and 6 (the whole string) contains all nucleotides,
  in particular nucleotide A whose impact factor is 1, so the answer is 1.
 Write a function:
 
-def solution(S, P, Q)
+    def solution(S, P, Q)
 
 that, given a non-empty zero-indexed string S consisting of N characters and
 two non-empty zero-indexed arrays P and Q consisting of M integers,
@@ -50,22 +50,20 @@ For example, given the string S = CAGCCTA and arrays P, Q such that:
 the function should return the values [2, 4, 1], as explained above.
 
 Assume that:
+    N is an integer within the range [1..100,000];
+    M is an integer within the range [1..50,000];
+    each element of arrays P, Q is an integer within the range [0..N − 1];
+    P[K] ≤ Q[K], where 0 ≤ K < M;
+    string S consists only of upper-case English letters A, C, G, T.
 
-N is an integer within the range [1..100,000];
-M is an integer within the range [1..50,000];
-each element of arrays P, Q is an integer within the range [0..N − 1];
-P[K] ≤ Q[K], where 0 ≤ K < M;
-string S consists only of upper-case English letters A, C, G, T.
 Complexity:
-
-expected worst-case time complexity is O(N+M);
-expected worst-case space complexity is O(N), beyond input storage
-(not counting the storage required for input arguments).
-Elements of input arrays can be modified.
+    expected worst-case time complexity is O(N+M);
+    expected worst-case space complexity is O(N), beyond input storage
+    (not counting the storage required for input arguments).
+    Elements of input arrays can be modified.
 """
 
 def solution(S, P, Q):
-    # write your code in Python 2.7
     N = len(S)
     M = len(P)
     result = [0] * M
