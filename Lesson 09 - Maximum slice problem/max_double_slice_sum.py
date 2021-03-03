258c172ef10c685c01001b8ef2a9da677efff4c0
DK <Dineshkarthik@users.noreply.github.com>
2021-03-03 14:52:55 +0100

    fix: xrange to range & maxint to maxsize

--- Lesson 09 - Maximum slice problem/max_double_slice_sum.py
+++ Lesson 09 - Maximum slice problem/max_double_slice_sum.py
@@ -61,14 +61,14 @@ import sys
 def solution(A):
     n = len(A)
     left = [0] * n
-    for i in xrange(1, n - 1):
+    for i in range(1, n - 1):
         left[i] = max(0, left[i - 1] + A[i])
 
     right = [0] * n
-    for i in xrange(n - 2, 1, -1):
+    for i in range(n - 2, 1, -1):
         right[i] = max(0, right[i + 1] + A[i])
 
-    max_sum = -sys.maxint
-    for i in xrange(1, n - 1):
+    max_sum = -sys.maxsize
+    for i in range(1, n - 1):
         max_sum = max(max_sum, left[i - 1] + right[i + 1])
     return max_sum
