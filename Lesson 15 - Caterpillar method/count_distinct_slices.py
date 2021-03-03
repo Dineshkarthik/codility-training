882eb938a19be4865ee3257d2a6755f5133ca98e
DK <Dineshkarthik@users.noreply.github.com>
2021-03-03 19:20:45 +0100

    FIX: division result from float to int

--- Lesson 15 - Caterpillar method/count_distinct_slices.py
+++ Lesson 15 - Caterpillar method/count_distinct_slices.py
@@ -60,16 +60,16 @@ def solution(M, A):
 
     for i, a in enumerate(A):
         if s[a] >= last:
-            result += (i - last) * (i - last + 1) / 2
+            result += (i - last) * (i - last + 1) // 2
             last = s[a] + 1
-            result -= (i - last) * (i - last + 1) / 2
+            result -= (i - last) * (i - last + 1) // 2
 
             if result > 1e9:
                 return int(1e9)
 
         s[a] = i
 
-    result += (N - last) * (N - last + 1) / 2
+    result += (N - last) * (N - last + 1) // 2
     if result > 1e9:
         return int(1e9)
     return result
